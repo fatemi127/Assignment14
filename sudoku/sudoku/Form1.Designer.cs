cf6b5311f08a78ecc495b6b3b7f8698d687a459b
fatemi127 <fatemi127@gmail.com>
2022-05-03 16:18:16 +0430

    Add files via upload

--- sudoku/sudoku/Form1.Designer.cs
+++ sudoku/sudoku/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_check = new System.Windows.Forms.Button();
             this.btn_new_game = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
@@ -48,7 +49,9 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
-            this.tableLayoutPanel1.Controls.Add(this.btn_new_game, 0, 9);
+            this.tableLayoutPanel1.Controls.Add(this.btn_check, 7, 9);
+            this.tableLayoutPanel1.Controls.Add(this.btn_new_game, 3, 9);
+            this.tableLayoutPanel1.ForeColor = System.Drawing.Color.Black;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 10;
@@ -63,19 +66,35 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(487, 406);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(534, 413);
             this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // btn_check
+            // 
+            this.btn_check.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_check.BackColor = System.Drawing.Color.White;
+            this.btn_check.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btn_check.Location = new System.Drawing.Point(416, 372);
+            this.btn_check.Name = "btn_check";
+            this.btn_check.Size = new System.Drawing.Size(53, 38);
+            this.btn_check.TabIndex = 1;
+            this.btn_check.Text = "check!✅";
+            this.btn_check.UseVisualStyleBackColor = false;
+            this.btn_check.Click += new System.EventHandler(this.btn_check_Click);
             // 
             // btn_new_game
             // 
-            this.btn_new_game.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.btn_new_game.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_new_game.Location = new System.Drawing.Point(3, 363);
+            this.tableLayoutPanel1.SetColumnSpan(this.btn_new_game, 2);
+            this.btn_new_game.Location = new System.Drawing.Point(180, 372);
             this.btn_new_game.Name = "btn_new_game";
-            this.btn_new_game.Size = new System.Drawing.Size(48, 40);
+            this.btn_new_game.Size = new System.Drawing.Size(112, 38);
             this.btn_new_game.TabIndex = 1;
-            this.btn_new_game.Text = "button1";
+            this.btn_new_game.Text = "Open";
             this.btn_new_game.UseVisualStyleBackColor = true;
             this.btn_new_game.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -83,7 +102,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 481);
+            this.ClientSize = new System.Drawing.Size(558, 488);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -97,6 +116,7 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button btn_new_game;
+        private System.Windows.Forms.Button btn_check;
     }
 }
 
--- sudoku/sudoku/Form1.cs
+++ sudoku/sudoku/Form1.cs
@@ -13,42 +13,136 @@ namespace sudoku
 {
     public partial class Form1 : Form
     {
+        TextBox[,] cells;
+
         public Form1()
         {
             InitializeComponent();
+            cells = new TextBox[9, 9];
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
                 {
-                    TextBox t = new TextBox();
-                    t.Multiline = true;
-                    t.TextAlign = HorizontalAlignment.Center;
-                    t.Font = new Font("Tahoma", 25);
-                    t.Anchor = AnchorStyles.Right | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Top;
-                    tableLayoutPanel1.Controls.Add(t, i, j);
+                    cells[i, j] = new TextBox();
+                    cells[i, j].Multiline = true;
+                    cells[i, j].TextAlign = HorizontalAlignment.Center;
+                    cells[i, j].Font = new Font("Tahoma", 20);
+                    //cells[i, j].BackColor = Color.LightCyan;
+                    cells[i, j].Anchor = AnchorStyles.Right | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Top;
+                    tableLayoutPanel1.Controls.Add(cells[i, j], i, j);
+                    cells[i, j].TextChanged += new System.EventHandler(this.cells_TextChanged);
+
                 }
             }
+
+
+
+            for (int i = 0; i < 9; i++)
+            {
+                //   if (i % 2 == 1)
+
+                for (int j = 0; j < 9; j++)
+                {
+                    if (i < 3 && j < 3 || i > 5 && j > 5
+                        || 
+                        i < 3 && j > 5 || i > 5 && j < 3
+                        ||
+                        i > 4 && i < 6 && j > 4 && j < 6
+                        ||
+                        i > 3 && i < 6 && j >4 && j < 6
+                        ||
+                        i > 2 && i < 6 && j > 4 && j < 6
+                        ||
+                        i > 4 && i < 6 && j > 3 && j < 6
+                        ||
+                        i > 4 && i < 6 && j > 2 && j < 6
+                        ||
+                        i > 3 && i < 6 && j > 2 && j < 6
+                        ||
+                        i > 2 && i < 6 && j > 2 && j < 6)
+                    {
+                        
+                        cells[i, j].BackColor = Color.LightSkyBlue;
+                    }
+
+                }
+                //  }
+
+            }
         }
+             
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
+        private void cells_TextChanged(object sender, EventArgs e)
+        {
+            string Spil = this.ActiveControl.Text.ToString();
+            if (Spil != "1" && Spil != "2" && Spil != "3" && Spil != "4" && Spil != "5" &&
+                Spil != "6" && Spil != "7" && Spil != "8" && Spil != "9" && Spil != string.Empty)
+            {
+                this.ActiveControl.Text = string.Empty;
+                MessageBox.Show("Please Enter Just Number 1~9. This TextBox don't Accept Other Key!" +
+                    "\nلطفاً فقط اعداد 1 الی 9 را وارد کنید. این فیلد کلید دیگری را قبول نمی کند ", "Enter InCorrect Key"
+                    , MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                this.ActiveControl.Text = string.Empty;
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog x = new OpenFileDialog();
+            reset();
             if (x.ShowDialog() == DialogResult.OK)
             {
                 string file_path = x.FileName;
                 StreamReader my_file_reader = new StreamReader(file_path);
                 string big_text = my_file_reader.ReadToEnd();
-                MessageBox.Show (big_text);
-                char[] my_seperators = { ' ', '\n' };
+                //MessageBox.Show (big_text);
+                char[] my_seperators = { ' ', '\n' ,'\r'};
+                big_text = big_text.Replace("\n", "");
 
                 string[] numbers = big_text.Split(my_seperators);
+                int index = 0;
+                for (int i = 0; i < 9; i++)
+
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (numbers[index] != "0")
+                        {
+                            cells[i, j].ReadOnly = true;
+                            cells[j,i].Text = numbers[index]; 
+                        }
+                        index++;
+                    }
+                    
+                }
             }
            
+        }
+        private void reset()
+        {
+            for (int i = 0; i < 9; i++)
+               
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    cells[i, j].ReadOnly = false;
+                    cells[i, j].Text = "";
+
+                }
+            }
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_check_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
